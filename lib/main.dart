a907892cf8ac55eeeac70c9ad742cc5a5cc6afd2
tngmait12 <tongmaitruongvu11@gmail.com>
2025-03-06 15:27:09 +0700

    Revert "tadaaaaaaaa"
    This reverts commit f5093bc0

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:v_store/features/authentication/screens/onboarding.dart';
-import 'app.dart';
-
+// bla bla bla j cx dc
 // flutter pub run flutter_native_splash:create --path=splash.yaml
 // flutter pub run flutter_native_splash:remove
 void main(){
@@ -19,6 +18,7 @@ class App extends StatelessWidget {
       theme: ThemeData(),
       darkTheme: ThemeData(),
       home: const OnBoardingScreen(),
+      // bla bla asdasdasdas
     );
   }
 }
